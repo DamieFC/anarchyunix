106a8b518635ac6321360ff89eaf622ed111857a
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-14 09:21:59 -0700

    Gotta commit shit
    Yeet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # anarchyunix
 Anarchy Unix. E.n.o.u.g.h. s.a.i.d
+## Building
+Can't yet
+## Why are all the comments so rude?
+Because I'm a 13 yr old having a sh\*tty day who has "Labor Day" by the Dead Milkmen stuck in their head. I don't even know who Jerry Lewis ***is***!!!
--- kernel/include/drivers/tty.h
+++ kernel/include/drivers/tty.h
@@ -1,4 +1,11 @@
 #ifndef _INCLUDE_DRIVERS_TTY_H
 #define _INCLUDE_DRIVERS_TTY_H
 
-#endif _INCLUDE_DRIVERS_TTY_H
+#include <stddef.h>
+ 
+void terminal_initialize(void);
+void terminal_putchar(char c);
+void terminal_write(const char* data, size_t size);
+void terminal_writestring(const char* data);
+ 
+#endif //_INCLUDE_DRIVERS_TTY_H
