cb3696778b5dd1d89eaadc74b4589473efc0e4d4
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-16 08:57:35 -0700

    :) pls work pls work pls work

--- kernel/drivers/tty/tty.c
+++ kernel/drivers/tty/tty.c
@@ -44,7 +44,7 @@ void terminal_scroll(int line) {
  
 	for(loop = line * (VGA_WIDTH * 2) + 0xB8000; loop < VGA_WIDTH * 2; loop++) {
 		c = loop;
-		loop - (VGA_WIDTH * 2) = c;
+		c = loop - (VGA_WIDTH * 2);
 	}
 }
 
