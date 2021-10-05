92fc1e9e7eea419249381becfc7fb29e6793e71b
GNUOverYooouu <dtortoise99@gmail.com>
2021-10-05 08:19:28 -0700

    Uh maybe fix also 13 days since last commit *evil laugh*

--- kernel/drivers/tty/tty.c
+++ kernel/drivers/tty/tty.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include "../../../lib/libc/include/string.h"
+#include <string.h>
 
 #include <drivers/tty.h>
 
@@ -19,7 +19,7 @@ static uint16_t* terminal_buffer;
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BROWN); // Official monke colors
 	terminal_buffer = VGA_MEMORY;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -38,41 +38,13 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
-void terminal_scroll(int line) {
-	int loop;
-	char c;
- 
-	for(loop = line * (VGA_WIDTH * 2) + 0xB8000; loop < VGA_WIDTH * 2; loop++) {
-		c = loop;
-		c = loop - (VGA_WIDTH * 2);
-	}
-}
-
-void terminal_delete_last_line() {
-	int x, *ptr;
- 
-	for(x = 0; x < VGA_WIDTH * 2; x++) {
-		ptr = 0xB8000 + (VGA_WIDTH * 2) * (VGA_HEIGHT - 1) + x;
-		*ptr = 0;
-	}
-}
-
 void terminal_putchar(char c) {
-	int line;
 	unsigned char uc = c;
- 
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
-		{
-			for(line = 1; line <= VGA_HEIGHT - 1; line++)
-			{
-				terminal_scroll(line);
-			}
-			terminal_delete_last_line();
-			terminal_row = VGA_HEIGHT - 1;
-		}
+			terminal_row = 0;
 	}
 }
 
