43036387d6514a68cdec8bfa0cc9751d2fdf1589
GNUOverYooouu <dtortoise99@gmail.com>
2021-10-06 09:35:15 -0700

    v0.01

--- kernel/init/main.c
+++ kernel/init/main.c
@@ -2,5 +2,6 @@
 
 void kmain(void) {
     terminal_writestring("You're not welcome to AnarchyUnix\n");
+    terminal_writestring("Version: 0.01\n");
     terminal_writestring("Anarchy burger!! Hold the government!\n");
 }
